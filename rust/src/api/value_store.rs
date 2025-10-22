09b6d36872cd8a5b8cdc9a726aa442a74eb85c8e
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-10-22 21:59:30 +0100

    New field

--- rust/src/api/value_store.rs
+++ rust/src/api/value_store.rs
@@ -1,5 +1,6 @@
 use once_cell::sync::Lazy;
 use std::{path::PathBuf, sync::Mutex};
+use crate::api::music_handler::SongMetadata;
 
 pub static MUSIC_FOLDER: Lazy<Mutex<String>> = Lazy::new(|| {
     Mutex::new(
@@ -13,4 +14,13 @@ pub static MUSIC_FOLDER: Lazy<Mutex<String>> = Lazy::new(|| {
 pub fn update_music_folder(f: String) {
     let mut g = MUSIC_FOLDER.lock().unwrap();
     *g = f;
+}
+
+pub static CURRENT_SONG: Lazy<Mutex<Option<SongMetadata>>> = Lazy::new(|| {
+    Mutex::new(None)
+});
+
+pub fn update_current_song(s: SongMetadata) {
+    let mut g = CURRENT_SONG.lock().unwrap();
+    *g = Some(s);
 }
