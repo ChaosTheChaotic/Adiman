f17f932f4335f66e6dae883cf8ddbe30999e5d3f
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-10-25 16:58:49 +0100

    Warning

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1450,7 +1450,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                               ),
                               SettingsTextField(
                                 title:
-                                    'Plugin R/W Directory (where can plugins read or write files/directories)',
+                                    'Plugin R/W Directory (where can plugins read or write files/directories (this works through symlinks too))',
                                 initialValue: _pluginRwDir,
                                 hintText: 'Enter plugin r/w directory path...',
                                 onChanged: _savePluginRwDir,
